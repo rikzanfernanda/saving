1c9c61033fddf3400d1dc60392f9cca47d7b6c07
Rikzan Fernanda <fernandarikzan@gmail.com>
2021-07-19 07:29:20 +0700

    info history

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -2,12 +2,19 @@
 @section('content')
 
 <div class="container-fluid">
-    <div class="row mb-3">
-        <div class="col-sm-6">
-            <h5 class="m-0">History Anda</h5>
+    <h5 class="mb-3">History Anda</h5>
+    <div class="info-box mb-2 bg-info">
+        <span class="info-box-icon d-md-flex"><i class="fas fa-inbox"></i></span>
+
+        <div class="info-box-content">
+            <p>
+                Tombol <i class="fas fa-trash-restore text-red"></i> (restore) tujuannya adalah ketika
+                Anda telah melakukan input pamsukan/pengeluaran dan Anda ingin mengembalikannya. Ini akan 
+                mengembalikan saldo dibank Anda seperti semula
+            </p>
         </div>
     </div>
-    
+
     <div class="card">
         <div class="card-body">
             <table id="dt_history" class="table table-bordered table-hover" style="width:100%">
